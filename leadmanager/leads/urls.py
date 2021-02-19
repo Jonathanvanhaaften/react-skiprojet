2f5bcb8e952317f5c5a74a6231b7c9ba459d9723
[Jonathan] <[Jonathanvanhaaften@gmail.com]>
2021-02-19 14:59:58 -0700

    basic basic crud api

--- leadmanager/leads/urls.py
+++ leadmanager/leads/urls.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from .api import LeadViewSet
 
 router = routers.DefaultRouter()
-routers.register('api/leads', LeadViewSet, 'leads')
+router.register('api/leads', LeadViewSet, 'leads')
 
 
 urlpatterns = router.urls
