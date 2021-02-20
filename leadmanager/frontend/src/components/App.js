8a2d86a1bc020b8dd5d9e4d641b77bf1422463ae
[Jonathan] <[Jonathanvanhaaften@gmail.com]>
2021-02-19 19:20:07 -0700

    react working

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,14 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
 import Header from './layout/Header'
 
 class App extends Component{
     render(){
-        return (
-        <Header />
-        )
+        return <h1>This App</h1>
     }
 };
 
-ReactDom.render(<App />, document.getElementById('app') )
+ReactDom.render(<App />, document.getElementById('app'));
