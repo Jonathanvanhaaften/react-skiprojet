d554ce9830ab9f571337960c114575357effdfa3
[Jonathan] <[Jonathanvanhaaften@gmail.com]>
2021-02-21 15:54:39 -0700

    added store adding reducer

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,16 +1,22 @@
 import React, { Component, Fragment } from 'react';
 import ReactDom from 'react-dom';
 import Header from './layout/header';
-import Dashboard from './leads/Dashboard'
+
+import Dashboard from './leads/Dashboard';
+
+import { Provider } from 'react-redux';
+import store from '../store';
 
 class App extends Component{
     render(){
         return (
-        <Fragment>
-            <Header />
-            <Dashboard />
-            <h1>This App</h1>
-        </Fragment>
+            <Provider store={store}>
+            <Fragment>
+                <Header />
+                <Dashboard />
+                <h1>This App</h1>
+            </Fragment>
+        </Provider>
         )
     }
 };
