e26d0b64d702e87468cb95acc2cd1279ab99fff7
[Jonathan] <[Jonathanvanhaaften@gmail.com]>
2021-02-21 14:28:28 -0700

    header showing and npm watch added

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import Header from './layout/header';
 
 
 class App extends Component{
     render(){
         return (
-        <h1>This App</h1>
+        <div>
+            <Header />
+            <h1>This App</h1>
+        </div>
         )
     }
 };
