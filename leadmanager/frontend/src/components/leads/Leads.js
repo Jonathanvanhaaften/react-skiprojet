df21dd9b9e8a9322112e7b4b5e18d9461021684f
[Jonathan] <[Jonathanvanhaaften@gmail.com]>
2021-02-21 18:09:37 -0700

    store set up

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getLeads } from '../../actions/leads'
 
 export class Leads extends Component {
+    static propTypes ={
+        leads: PropTypes.array.isRequired
+    };
+    componentDidMount(){
+        this.props.getLeads();
+    }
     render() {
         return (
             <div>
                 <h1>Leads</h1>
             </div>
-        )
-    }
-}
+        )}
+};
+const mapStateToProps = state => ({
+        leads: state.leads.leads
+});
 
-export default Leads
+export default connect(mapStateToProps, {getLeads})(Leads);
--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux'
-import leads from "./leads"
+import { combineReducers } from 'redux';
+import leads from "./leads";
 
 export default combineReducers({
     leads
