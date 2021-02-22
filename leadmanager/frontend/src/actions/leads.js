a27f4a0750862a1053e49c2a076f0dd6cc03d3c4
[Jonathan] <[Jonathanvanhaaften@gmail.com]>
2021-02-22 12:42:59 -0700

    backtics for the win we can now delete

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-import { GET_LEADS } from './types'
+import { GET_LEADS, DELETE_LEAD } from './types'
 
 // GET LEADS
 export const getLeads = () => dispatch =>{
-    axios.get('/api/leads')
+    axios.get('/api/leads/')
         .then(res => {
             dispatch({
                 type: GET_LEADS,
@@ -12,4 +12,15 @@ export const getLeads = () => dispatch =>{
             });
         }).catch(err => console.log(err));
 
+};
+// DELETE LEAD
+export const deleteLead = (id)=> dispatch =>{
+ 
+    axios.get(`/api/leads/${id}/`)
+        .then(res => {
+            dispatch({
+                type: DELETE_LEAD,
+                payload: id
+            });
+        })
 }
